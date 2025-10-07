4c414183e9974bb7ffe89bfa69e3922487c22c4c
Yi Sheng Lee <leeyisheng14@hotmail.com>
2025-10-07 19:33:12 +0800

    docs(Command): add Javadoc comments for execute() parameters and isExit()

--- src/main/java/seedu/mama/command/Command.java
+++ src/main/java/seedu/mama/command/Command.java
@@ -3,15 +3,18 @@ package seedu.mama.command;
 import seedu.mama.model.EntryList;
 import seedu.mama.storage.Storage;
 
-/**
- * A user-invoked action.
- */
 public interface Command {
     /**
-     * Executes and returns a user-visible message.
+     * Executes the command.
+     *
+     * @param lists The EntryList to operate on.
+     * @param storage The Storage.
      */
-    String execute(EntryList list, Storage storage);
+    String execute(EntryList lists, Storage storage);
 
+    /**
+     * Whether this command should cause the program to exit.
+     */
     default boolean isExit() {
         return false;
     }
