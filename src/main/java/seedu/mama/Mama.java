8c0a896c3685b105bcc5051f484e525471d44704
ryan <e1156608@u.nus.edu>
2025-10-10 18:25:25 +0800

    space entered

--- src/main/java/seedu/mama/Mama.java
+++ src/main/java/seedu/mama/Mama.java
@@ -14,7 +14,8 @@ public class Mama {
     public static void main(String[] args) {
         System.out.println("Hello from MAMA");
 
-        System.out.println("Enter a command (workout <description> /dur <duration>, delete <index>, list, milk <volume>, bye)");
+        System.out.println("Enter a command (workout <description> /dur <duration>, " +
+                "delete <index>, list, milk <volume>, bye)");
 
         Storage storage = Storage.defaultStorage();
         EntryList list = storage.loadOrEmpty();
