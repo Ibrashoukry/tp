7188831c966372f7dc3a1156bb1ca57cc3f57452
ryan <e1156608@u.nus.edu>
2025-10-10 17:01:27 +0800

    Edited PR errors

--- src/main/java/seedu/mama/Mama.java
+++ src/main/java/seedu/mama/Mama.java
@@ -14,7 +14,8 @@ public class Mama {
     public static void main(String[] args) {
         System.out.println("Hello from MAMA");
 
-        System.out.println("Enter a command (workout <description> /dur <duration>, delete <index>, list, weight <weight>, bye)");
+        System.out.println("Enter a command (workout <description> /dur <duration>, delete <index>, list, " +
+                "weight <weight>, bye)");
 
         Storage storage = Storage.defaultStorage();
         EntryList list = storage.loadOrEmpty();
