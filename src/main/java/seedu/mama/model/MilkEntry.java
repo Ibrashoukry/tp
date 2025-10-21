219e9816853d1736dd5d2d9b704822ed0206e4e9
ryan <e1156608@u.nus.edu>
2025-10-21 15:56:07 +0800

    MilkEntry: Add totalMilkVol variable for tracking

--- src/main/java/seedu/mama/model/MilkEntry.java
+++ src/main/java/seedu/mama/model/MilkEntry.java
@@ -1,7 +1,27 @@
 package seedu.mama.model;
 
 public class MilkEntry extends Entry {
-    private int totalMilkVol;
+    private static int totalMilkVol;
+
+    public static void addTotalMilkVol(int MilkVol) {
+        totalMilkVol += MilkVol;
+    }
+
+    public static int getMilkVol(String volFromStorage) {
+        if (volFromStorage.endsWith("ml")) {
+            volFromStorage = volFromStorage.substring(0, volFromStorage.length() - 2);
+        }
+        return Integer.parseInt(volFromStorage);
+    }
+
+    public static void updateTotalMilkVol(String line) {
+        int milkVol = getMilkVol(line);
+        addTotalMilkVol(milkVol);
+    }
+
+    public static String toTotalMilk() {
+        return "Total breast milk pumped: " + totalMilkVol + "ml";
+    }
 
     public MilkEntry(String text) {
         super("MILK", text);
