960100b6ffb215805a92794b58f7d30c77aa0f6d
ryan <e1156608@u.nus.edu>
2025-10-21 17:18:08 +0800

    AddMilkCommand: Add Logger

--- src/main/java/seedu/mama/command/AddMilkCommand.java
+++ src/main/java/seedu/mama/command/AddMilkCommand.java
@@ -6,11 +6,17 @@ import seedu.mama.model.MilkEntry;
 import seedu.mama.storage.Storage;
 import seedu.mama.ui.Ui;
 
+import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Adds how much breast milk has been pumped and returns the user
  * the amount of breast milk pumped in ml
  */
 public class AddMilkCommand implements Command {
+    // 1. Set up logger for this class
+    private static final Logger LOG = Logger.getLogger(AddMilkCommand.class.getName());
 
     public final int milkVolume;
     // Only accept positive volume
@@ -23,6 +29,11 @@ public class AddMilkCommand implements Command {
 
     @Override
     public String execute(EntryList list, Storage storage) {
+        // Log the start of the execution
+        LOG.log(Level.INFO, "Executing AddMilkCommand.");
+
+        // Use assertion to check for internal errors
+        // Confirms that milkVolume is greater than 0
         assert this.milkVolume > 0 : "The milkVolume must be greater than 0!";
 
         MilkEntry.addTotalMilkVol(milkVolume);
@@ -33,6 +44,7 @@ public class AddMilkCommand implements Command {
             storage.save(list);
         }
 
+        LOG.log(Level.INFO, "AddMilkCommand successfully executed, adding: " + milkVolume + "ml");
         return "Breast Milk Pumped: " + newMilk.toListLine() + "\n" +  MilkEntry.toTotalMilk();
     }
 }
