c0430c31f7f03bfeda34560d3caa73b002dd4db3
ryan <e1156608@u.nus.edu>
2025-10-21 15:27:01 +0800

    Parser: Remove weight duplicate condition

--- src/main/java/seedu/mama/parser/Parser.java
+++ src/main/java/seedu/mama/parser/Parser.java
@@ -66,10 +66,11 @@ public class Parser {
                 return (l, s) -> "Weight must be a number. Try `weight`+ 'value of weight'";
             }
         }
+        
         if (trimmed.startsWith("meal")) {
             return AddMealCommand.fromInput(trimmed);
         }
-        
+
         return (l, s) -> "Unknown command.";
     }
 }
