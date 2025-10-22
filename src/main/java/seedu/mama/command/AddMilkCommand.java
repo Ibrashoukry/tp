7bb9bd85df21580c1a01511361f3a0e42ef7cf8b
ryan <e1156608@u.nus.edu>
2025-10-22 11:28:06 +0800

    AddMIlkCommand: remove date as a parameter

--- src/main/java/seedu/mama/command/AddMilkCommand.java
+++ src/main/java/seedu/mama/command/AddMilkCommand.java
@@ -21,52 +21,26 @@ public class AddMilkCommand implements Command {
     private static final Logger LOG = Logger.getLogger(AddMilkCommand.class.getName());
 
     public final int milkVolume;
-    public final LocalDate dateOfPump;
+
     // Only accept positive volume
-    public AddMilkCommand(int milkVolume, LocalDate dateOfPump) throws CommandException {
+    public AddMilkCommand(int milkVolume) throws CommandException {
         if (milkVolume < 0) {
             throw new CommandException("milkVolume must be a positive number!");
         }
         this.milkVolume = milkVolume;
-        this.dateOfPump = dateOfPump;
     }
 
-    public static LocalDate parseDate(String date) throws CommandException {
-        if (!date.matches("\\d{4}/\\d{2}/\\d{2}")) {
-            throw new CommandException("Invalid date format!");
-        }
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyy/MM/dd");
-        try {
-            return LocalDate.parse(date, formatter);
-        } catch (DateTimeParseException ex) {
-            throw new CommandException("Please enter a valid date in YYYY/MM/DD format.");
-        }
-    }
 
     public static AddMilkCommand fromInput(String input) throws CommandException {
         String desc = input.substring("milk".length()).trim();
 
-        if (!desc.contains("/date")) {
-            throw new CommandException("Invalid date format! Try: milk <volume> /date <date>");
-        }
-
-        String[] parts = desc.split("/date");
-        if (parts.length != 2) {
-            throw new CommandException("Invalid date format! Try: milk <volume> /date <date>");
-        }
-
-        String volString = parts[0].trim();
-        String dateOfPump = parts[1].trim();
-        LocalDate date = parseDate(dateOfPump);
-
         int milkVolume;
         try {
-            milkVolume = Integer.parseInt(volString);
+            milkVolume = Integer.parseInt(desc);
         } catch (NumberFormatException e) {
             throw new CommandException("Volume must be a number");
         }
-        return new AddMilkCommand(milkVolume, date);
+        return new AddMilkCommand(milkVolume);
     }
 
     @Override
@@ -81,7 +55,7 @@ public class AddMilkCommand implements Command {
         // increment the total milk volume everytime
         MilkEntry.addTotalMilkVol(milkVolume);
 
-        Entry newMilk = new MilkEntry(milkVolume + "ml", dateOfPump);
+        Entry newMilk = new MilkEntry(milkVolume + "ml");
         list.add(newMilk);
         if (storage != null) {
             storage.save(list);
